0af682f0f63bec90b23ab04f1e3598fc495cb57c
MatanLank <111992346+MatanLank@users.noreply.github.com>
2022-10-27 09:47:59 +0300

    Add files via upload

---
+++ SSO RDP.ps1
@@ -0,0 +1,13 @@
+﻿    mstsc /v asctxtes001
+
+    Start-Sleep -s 7
+     
+    [System.Windows.Forms.SendKeys]::SendWait("{matanla}")
+
+    Start-Sleep -s 7
+
+    [System.Windows.Forms.SendKeys]::SendWait("{1}")
+
+    Start-Sleep -s 7
+
+    [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
