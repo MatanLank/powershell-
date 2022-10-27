2d9ad6e51d00e475ebe6b1416f949734f63490b2
MatanLank <111992346+MatanLank@users.noreply.github.com>
2022-10-27 09:16:12 +0300

    Add files via upload

---
+++ NEWinstallWorkspace.bat
@@ -0,0 +1,4 @@
+rem "\\kits-netapp1\installs$\Workstations\Citrix\CItrix Workspace 2109\CitrixWorkspaceApp.exe"  /silent /uninstall
+"\\kits-netapp1\installs$\Workstations\Citrix\CItrix Workspace 2109\\\kits-netapp1\installs$\Workstations\Citrix\CItrix Workspace 2109\MicrosoftEdgeWebView2RuntimeInstallerX64.exe" /silent /includeSSON /AllowAddStore=N /rcu
+"\\kits-netapp1\installs$\Workstations\Citrix\CItrix Workspace 2109\CitrixWorkspaceNEW.exe" /silent /includeSSON /AllowAddStore=N /rcu
+exit
