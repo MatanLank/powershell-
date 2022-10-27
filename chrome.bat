090cccad661529dfc4bfeb0f258debf38b9c5599
MatanLank <111992346+MatanLank@users.noreply.github.com>
2022-10-27 09:33:56 +0300

    Add files via upload

---
+++ chrome.bat
@@ -0,0 +1,15 @@
+Dim iURL
+Dim objShell
+Dim SendKeys
+
+iURL = "https://storefront.asuta.co.il/Citrix/AsutaWeb/"
+
+set objShell = CreatObject("Shell.Application")
+objShell.ShellExecute "chrome.exe", iURL, "https://storefront.asuta.co.il/Citrix/AsutaWeb/"
+
+WSH.Sleep 4000
+
+Set WshShell = WScript.CreateObject("WScript.Shell")
+WshShell.SendKeys "{ENTER}"
+
+
