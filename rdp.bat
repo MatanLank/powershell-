1882b69a5b195b846bf3d1f82690909a13e05b98
MatanLank <111992346+MatanLank@users.noreply.github.com>
2022-10-27 11:06:55 +0300

    Add files via upload

---
+++ rdp.bat
@@ -0,0 +1,17 @@
+   mstsc /v asctxtes001
+
+   Start-Sleep -s 5
+
+    [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+
+    Start-Sleep -s 5
+     
+    [System.Windows.Forms.SendKeys]::SendWait("{matanla}")
+
+    Start-Sleep -s 5
+
+    [System.Windows.Forms.SendKeys]::SendWait("{Bp19@19!}")
+
+    Start-Sleep -s 5
+
+    [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
